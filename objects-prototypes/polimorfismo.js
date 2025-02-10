641885494d86ecbf8093035e4999cce13a9d5501
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-02-09 21:22:17 -0300

    metodos adicionados para conta

--- objects-prototypes/polimorfismo.js
+++ objects-prototypes/polimorfismo.js
@@ -3,3 +3,28 @@ function Account(agency, account, balance){
     this.account = account
     this.balance = balance
 }
+
+Account.prototype.withdraw = function(value) {
+    if (this.balance < value) {
+        this.getBalance()
+        return;
+    }
+
+    this.balance -= value
+    this.getBalance()
+}
+Account.prototype.deposit = function(value) {
+    this.balance += value
+    this.getBalance()
+}
+Account.prototype.getBalance = function() {
+    console.log(
+        `Ag/C: ${this.agency} / ${this.account} Balance: ${this.balance}`
+    )
+}
+
+const account1 = new Account(11, 22, 1000)
+
+account1.getBalance()
+account1.withdraw(10)
+
