524e51f85c60b87df7ccfcafd23798a3872abfaa
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-03-19 21:21:15 -0300

    finalizando validação de cpf

--- poo/validar_cpf.js
+++ poo/validar_cpf.js
@@ -9,11 +9,27 @@ class ValidaCPF {
     }
 
     ehSequencia() {
-        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length)
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
     }
 
     geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2
+    }
 
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let stringNumerica of cpfSemDigitos) {
+            total += reverso * Number(stringNumerica)
+            reverso--
+        }
+
+        const digito = 11 - (total % 11)
+        return digito <= 9 ? String(digito) : '0'
     }
 
     valida() {
@@ -21,10 +37,17 @@ class ValidaCPF {
         if(typeof this.cpfLimpo !== 'string') return false;
         if (this.cpfLimpo.length !== 11) return false;
         if (this.ehSequencia()) return false;
-        if(!this.geraNovoCpf()) return false;
-        return 'cheguei'
+        this.geraNovoCpf()
+        
+        return this.novoCPF === this.cpfLimpo
     }
 }
 
 let validaCPF = new ValidaCPF('09497255365')
 console.log(validaCPF.valida())
+
+if (validaCPF.valida()) {
+    console.log('valido')
+} else {
+    console.log('nn valido')
+}
