8899d29daee4e7ff803e26ea8eede5b830701faa
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-03-25 20:02:12 -0300

    aprendendo metodos de promises

--- assincorono/promises.js
+++ assincorono/promises.js
@@ -7,23 +7,42 @@ function rand(min, max) {
 function esperaAi(msg, tempo){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(msg);
+            resolve(msg.toUpperCase() +  ' passei na promisse');
         }, tempo);
     });
 }
 
-esperaAi('msg 1', rand(1, 3))
-    .then(resposta => {
-        console.log(resposta)
-        return esperaAi('msg 2', rand(1, 3))
+// esperaAi('msg 1', rand(1, 3))
+//     .then(resposta => {
+//         console.log(resposta)
+//         return esperaAi('msg 2', rand(1, 3))
+//     })
+//     .then(resposta => {
+//         console.log(resposta)
+//         return esperaAi('msg 3', rand(1, 3))
+//     })
+//     .then(resposta => {
+//         console.log(resposta)
+//     }).then(() => {
+//         console.log('ultimo a ser exibido')
+//     })
+//     .catch(e => {
+//         console.log(e)
+//     })
+
+
+const promises = [
+    'primeiro valor',
+    esperaAi('promise 1', 3000),
+    esperaAi('promise 2', 500),
+    esperaAi('promise 3', 1000),
+    'outro valor'
+]
+
+Promise.all(promises)
+    .then(function(valor) {
+        console.log(valor)
     })
-    .then(resposta => {
-        console.log(resposta)
-        return esperaAi('msg 3', rand(1, 3))
+    .catch( function(e) {
+        console.log(e)
     })
-    .then(resposta => {
-        console.log(resposta)
-    }).then(() => {
-        console.log('ultimo a ser exibido')
-    })
-    .catch()
